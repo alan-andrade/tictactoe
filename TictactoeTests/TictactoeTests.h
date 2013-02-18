a311b3fa030c803edf46585f5fca60cff618f1fb
Gallo <gallo@saleyvale.local>
2013-02-17 19:40:28 -0600

    Testing Skeleton

---
+++ TictactoeTests/TictactoeTests.h
@@ -0,0 +1,13 @@
+//
+//  TictactoeTests.h
+//  TictactoeTests
+//
+//  Created by Gallo on 17/02/13.
+//  Copyright (c) 2013 PartyMan. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TictactoeTests : SenTestCase
+
+@end
---
+++ TictactoeTests/TictactoeTests.m
@@ -0,0 +1,32 @@
+//
+//  TictactoeTests.m
+//  TictactoeTests
+//
+//  Created by Gallo on 17/02/13.
+//  Copyright (c) 2013 PartyMan. All rights reserved.
+//
+
+#import "TictactoeTests.h"
+
+@implementation TictactoeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TictactoeTests");
+}
+
+@end
